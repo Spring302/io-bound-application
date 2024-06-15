be7f8b16c6aab5464c5529e0c2abd5330b4f3036
Spring302 <gkwoduf08@naver.com>
2024-06-15 23:40:33 +0900

    첫페이지 캐시 저장(SQL Load 감소)

--- src/main/java/class101/foo/io/Consumer.java
+++ src/main/java/class101/foo/io/Consumer.java
@@ -3,23 +3,23 @@ package class101.foo.io;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Consumer {
 
-    @Autowired
-    ObjectMapper objectMapper;
+  @Autowired
+  ObjectMapper objectMapper;
 
-    @Autowired
-    PostRepository postRepository;
+  @Autowired
+  PostRepository postRepository;
 
-    @RabbitListener(queues = "CREATE_POST_QUEUE")
-    public void handler(String message) throws JsonProcessingException {
-        // message를 Post의 class 타입으로 변환 후 objectMapper로 읽은 후 Save
-        Post post = objectMapper.readValue(message,Post.class);
-        postRepository.save(post);
-    }
+  @SuppressWarnings("null")
+  @RabbitListener(queues = "CREATE_POST_QUEUE")
+  public void handler(String message) throws JsonProcessingException {
+    // message를 Post의 class 타입으로 변환 후 objectMapper로 읽은 후 Save
+    Post post = objectMapper.readValue(message, Post.class);
+    postRepository.save(post);
+  }
 }
--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -2,55 +2,64 @@ package class101.foo.io;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class PostController {
 
-    private static Integer PAGE_SIZE = 20;
+  private static Integer PAGE_SIZE = 20;
 
-    @Autowired
-    PostRepository postRepository;
+  @Autowired
+  PostRepository postRepository;
 
-    @Autowired
-    Producer producer;
+  @Autowired
+  Producer producer;
 
-    @Autowired
-    ObjectMapper objectMapper;
+  @Autowired
+  ObjectMapper objectMapper;
 
-    // 1. 글을 작성한다.
-    @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) throws JsonProcessingException {
-        String jsonPost = objectMapper.writeValueAsString(post);
-        producer.sendTo(jsonPost);
-        return post;
+  @Autowired
+  PostCacheService postCacheService;
+
+  // 1. 글을 작성한다.
+  @PostMapping("/post")
+  public Post createPost(@RequestBody Post post)
+    throws JsonProcessingException {
+    String jsonPost = objectMapper.writeValueAsString(post);
+    producer.sendTo(jsonPost);
+    return post;
+  }
+
+  // 2 글 목록을 페이징하여 반환
+  @GetMapping("/posts")
+  public Page<Post> getPostList(
+    @RequestParam(defaultValue = "1") Integer page
+  ) {
+    if (page.equals(1)){
+      return postCacheService.getFirstPostPage();
+    } else {
+      return postRepository.findAll(
+              // 받아오는 page는 0부터 시작하는데 실제 요청은 1부터 해야하는 것 같다(확인필요)
+              PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+      );
     }
+  }
 
-    // 2 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        return postRepository.findAll(
-                // 받아오는 page는 0부터 시작하는데 실제 요청은 1부터 해야하는 것 같다(확인필요)
-                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-        );
-    }
-    
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
-    }
-    
-    // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
-    @GetMapping("/search")
-    public List<Post> findPostsByContent(@RequestParam String content){
-        return postRepository.findByContentContains(content);
-    }
+  // 3. 글 번호로 조회
+  @SuppressWarnings("null")
+  @GetMapping("/post/{id}")
+  public Post getPostById(@PathVariable("id") Long id) {
+    return postRepository.findById(id).get();
+  }
 
+  // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
+  @GetMapping("/search")
+  public List<Post> findPostsByContent(@RequestParam String content) {
+    return postRepository.findByContentContains(content);
+  }
 }
